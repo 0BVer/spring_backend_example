8fec3c6f714ec70bed4173bc94d7df1867e2fb80
0BVer <dr0joon@gmail.com>
2022-12-03 02:19:34 +0900

    init : initial commit

---
+++ src/main/java/com/example/spring_backend_example/SpringBackendExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_backend_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackendExampleApplication.class, args);
+    }
+
+}
